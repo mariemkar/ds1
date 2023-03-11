56376664866a65b6fb8d2df2bec79e686e19f2d5
mariemkar <126613859+mariemkar@users.noreply.github.com>
2023-03-11 21:32:08 +0100

    Add files via upload

---
+++ aa.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>header</title>
+	<link rel="stylesheet" type="text/css" href="ss.css">
+</head>
+<body>
+<div class="header">
+    <div class="header-top">
+
+       <label class="logo">FRESH FLOWERS </label>  
+    </div> 
+    
+ <nav>
+  
+ 	<ul class="menu">
+ 		<li><a href="acceuil.php">Acceuil</a>
+         </li>
+         <li><a href="boutique.php">Boutique</a></li>
+         <li><a href="inscription.php">Inscription</a></li>
+         <li> <a href="connexion.php">Connexion</a></li>
+         <li><a href="panier.php">panier</a></li>
+ 	</ul>
+<ul >
+ 	<form>
+        <input type="search" name="rechercher" placeholder="search" >
+        <button class="btn btn-outline-success" type="submit" >Search</button>
+    </form>
+     
+          <li>
+         <a href="connexion.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg> </a>
+  </li>
+
+     <li>
+  <a href="panier.php" class="">               
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+  </svg> 
+ </a></li>
+ </ul>  	
+
+ </nav>
+</div>
+
+
+</body>
+</html>
